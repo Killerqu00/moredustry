97750fd523f632e114f0235d70d84cad8e7d94bf
Ivan <Killerqueen1777@gmail.com>
2020-03-04 17:51:25 +0300

    Alpha38

---
+++ scripts/warp.js
@@ -0,0 +1,20 @@
+const silo = extendContent(Block, "scatter-silo", {
+
+    // override the method to build configuration
+    buildConfiguration(tile, table) {
+        table.addImageButton(
+            Icon.arrowUpSmall,
+            Styles.clearTransi,
+
+            // configure the tile to signal that it has been
+            // pressed (this sync on client to server)
+            run(() => tile.configure(0))
+        ).size(50);
+    },
+
+    // override configure event
+    configured(tile, value) {
+        Calls.damage(tile.x, tile.y, 100.0, 1);
+        //null
+    }
+});
